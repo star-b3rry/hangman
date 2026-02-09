6fb2c189c5d838d7294c051d2dca589668a8551d
star-b3rry <scarlette.adamic@ucas-edu.net>
2026-02-09 13:43:53 -0700

    Co-authored-by: Kasen659 <Kasen659@users.noreply.github.com> Co-authored-by: fr3d-the-7th <fr3d-the-7th@users.noreply.github.com>

--- team_game.py
+++ team_game.py
@@ -1,6 +1,24 @@
-words1 = ["hello", "world"]#five leters
+words = ["hello", "world", "jump", "flip", "done", "game", "team", "dog", "cat", "run", "rip", "gum", "therapy"]
+import random #words
+print(words)
+letters = input
 
-words2 = ["jump", "flip", "done", "game", "team"]#four leters
+print('Welcome player! try to guess the word before the hangman is complete. You have 6 wrong guesses.')
+print('______')
+print('|    )
+print("|")
+print("|")
+print("------")
+guess = input('What letter is your first guess?: ').strip().capitalize()
+def guess (a):
+    if guess == 'a', 
 
-words3 = ["dog", "cat", "run", "rip", "gum"]#three leters
 
+
+# our dude
+
+ O
+/|\\
+/ \\
+
+if letters ==
