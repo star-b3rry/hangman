1412e6816ad80a866f5852c511ceef0485d01923
star-b3rry <scarlette.adamic@ucas-edu.net>
2026-02-11 13:43:19 -0700

    efeauoufaoyfaoiyewaouy

--- team_game.py
+++ team_game.py
@@ -11,26 +11,76 @@ guesses = []
 print(word)
 #hope
 print('Welcome player! Try to guess the word before the hangman is complete. You have 6 wrong guesses.')
-#kasen
+#kasen, hope
 wrong = 0
 while True:
-    blan
+    blanks = ""
     for letter in word:
       if letter in guesses:
-          print(letter)
-      elif:
-          print("_")
+          blanks += letter
+      else:
+          blanks += "_"
+    if letter not in guesses:
+        wrong += 1
 
       
-
+    print(blanks)
     letter1 = input('What letter is your guess?: ').strip().lower()
     guesses.append(letter1)
-    print('______')
-    print('|    |')
-    print("|")
-    print("|")
-    print("|")
-    print("-----")
+    #hope, kasen, scarlette- changes the hangman
+    if wrong == 0:
+      print('______')
+      print('|    |')
+      print("|")
+      print("|")
+      print("|")
+      print("-----")
+    elif wrong == 1:
+      print('______')
+      print('|    |')
+      print("|    0")
+      print("|")
+      print("|")
+      print("-----")
+    elif wrong == 2:
+      print('______')
+      print('|    |')
+      print("|    0")
+      print("|    |")
+      print("|")
+      print("-----")
+    elif wrong == 3:
+      print('______')
+      print('|    |')
+      print("|    0")
+      print("|   /|")
+      print("|")
+      print("-----")
+    elif wrong == 4:
+      print('______')
+      print('|    |')
+      print("|    0")
+      print("|   /|\\")
+      print("|")
+      print("-----")
+    elif wrong == 5:
+      print('______')
+      print('|    |')
+      print("|    0")
+      print("|   /|\\")
+      print("|   / ")
+      print("-----")
+    elif wrong == 6:
+      print('______')
+      print('|    |')
+      print("|    0")
+      print("|   /|\\")
+      print("|   / \\")
+      print("-----")
+    else:
+      print("GAME OVER!")
+    break
+
     #hope
 
 
